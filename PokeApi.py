782687d3f9afaef9dc5ed4af5fb386b9aea1d74a
Yagyu27 <arnovisdee@hotmail.com>
2020-05-12 11:50:38 +0100

    Stats Base

--- PokeApi.py
+++ PokeApi.py
@@ -26,8 +26,11 @@ def get_pokemons(url, offset, pokename):
                     payload2= response2.json()
                     types= payload2.get('types', [])
                     abilities= payload2.get('abilities', [])
+                    stats= payload2.get('stats', [])
                     tipo1=0
                     habilidad=0
+                    stabs=0
+                    valor=0
                     oculta= False
                     si= '-Habilidad oculta-'
 
@@ -46,6 +49,13 @@ def get_pokemons(url, offset, pokename):
                         for tipos in types:
                             tipo1= tipos['type']
                             print(tipo1['name'])
+                    
+                    print("\n***Stats Base***")
+                    if stats:
+                        for base in stats:
+                            valor= base['base_stat']
+                            stabs= base['stat']
+                            print(stabs['name'], ": ",valor)
         
         if(encontrado==0):
             offset= offset+20
