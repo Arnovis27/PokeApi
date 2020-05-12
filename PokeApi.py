5ea99cd3c310b9200e67b97d2bebc88312259d48
Yagyu27 <arnovisdee@hotmail.com>
2020-05-12 12:26:10 +0100

    Ajustes

--- PokeApi.py
+++ PokeApi.py
@@ -1,74 +1,55 @@
 import requests
 
-def get_pokemons(url, offset, pokename):
+def get_pokemons(url, pokename):
 
-    args= {'offset':offset} if offset else {}
-
-    response= requests.get(url, params= args)
-    name=[]
-    encontrado=0
-
+    url2= url + pokename
+    response= requests.get(url2)
+  
     if response.status_code == 200:
-        payload= response.json()
-        results= payload.get('results', [])
+        response2= requests.get(url2)
+        payload2= response2.json()
+        types= payload2.get('types', [])
+        abilities= payload2.get('abilities', [])
+        stats= payload2.get('stats', [])
+        tipo1=0
+        habilidad=0
+        stabs=0
+        valor=0
+        oculta= False
+        si= '-Habilidad oculta-'
 
-        if results:
-            for pokemon in results:
-                name.append(pokemon['name'])
-        
-        for i in range(len(name)):
-            if name[i] == pokename:
-                encontrado=1
-                print("\n¡Existe en la base de datos!")
-                url2= 'http://pokeapi.co/api/v2/pokemon/' + pokename
-                if response.status_code == 200:
-                    response2= requests.get(url2)
-                    payload2= response2.json()
-                    types= payload2.get('types', [])
-                    abilities= payload2.get('abilities', [])
-                    stats= payload2.get('stats', [])
-                    tipo1=0
-                    habilidad=0
-                    stabs=0
-                    valor=0
-                    oculta= False
-                    si= '-Habilidad oculta-'
+        print("\n\t", pokename.upper())
 
-                    print("\n***Habilidades***")
-                    if abilities:
-                        for hiden in abilities:
-                            oculta= hiden['is_hidden']
-                            habilidad= hiden['ability']
-                            if(oculta== True):
-                                print(habilidad['name'],si)
-                            else:
-                                print(habilidad['name'])
+        print("\n***Tipo***")
+        if types:
+            for tipos in types:
+                tipo1= tipos['type']
+                print(tipo1['name'])
 
-                    print("\n***Tipo***")
-                    if types:
-                        for tipos in types:
-                            tipo1= tipos['type']
-                            print(tipo1['name'])
+        print("\n***Habilidades***")
+        if abilities:
+            for hiden in abilities:
+                oculta= hiden['is_hidden']
+                habilidad= hiden['ability']
+                if(oculta== True):
+                    print(habilidad['name'],si)
+                else:
+                    print(habilidad['name'])
                     
-                    print("\n***Stats Base***")
-                    if stats:
-                        for base in stats:
-                            valor= base['base_stat']
-                            stabs= base['stat']
-                            print(stabs['name'], ": ",valor)
-        
-        if(encontrado==0):
-            offset= offset+20
-            get_pokemons(url,offset,pokename) 
+        print("\n***Stats Base***")
+        if stats:
+            for base in stats:
+                valor= base['base_stat']
+                stabs= base['stat']
+                print(stabs['name'], ": ",valor)
 
-         
-
+    else:
+        print("\nMal escrito o Pokemon no existente en base de datos.")
 
 
 if __name__ == "__main__":
-    url='http://pokeapi.co/api/v2/pokemon-form/'
-    offset=0
+    url='http://pokeapi.co/api/v2/pokemon/'
     nombrepokemon=str(input("\nNombre del Pokemon: "))
     pokename= nombrepokemon.lower()
 
-    get_pokemons(url,offset,pokename)
+    get_pokemons(url,pokename)
